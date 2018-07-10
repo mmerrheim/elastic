a3099a383850d2e7dbc91eef6284da4a62279e3c
Merrheim <mmerrheim@atecna.fr>
2018-07-10 14:42:17 +0200

    first commit

---
+++ elasticsearch-bundle/Annotation/Object.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\ElasticsearchBundle\Annotation;
+
+/**
+ * Annotation to mark a class as an object during the parsing process.
+ *
+ * `Object` name as class name is forbidden in PHP 7 but we never create this
+ *  class as object and only use it for annotation definition.
+ *
+ * @Annotation
+ * @Target("CLASS")
+ */
+final class Object1
+{
+    const NAME = 'object';
+
+    /**
+     * In this field you can define options.
+     *
+     * @var array
+     */
+    public $options = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function dump(array $exclude = [])
+    {
+        return array_diff_key(
+            $this->options,
+            $exclude
+        );
+    }
+}
---
+++ elasticsearch-bundle/Command/CacheClearCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\ElasticsearchBundle\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Symfony command for clearing elasticsearch cache.
+ */
+class CacheClearCommand extends AbstractManagerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+            ->setName('ongr:es:cache:clear')
+            ->setDescription('Clears elasticsearch client cache.');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $this
+            ->getManager($input->getOption('manager'))
+            ->clearCache();
+        $io->success(
+            sprintf(
+                'Elasticsearch index cache has been cleared for manager named `%s`',
+                $input->getOption('manager')
+            )
+        );
+    }
+}
